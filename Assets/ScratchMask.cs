905e4788276e5c049178e053b6f64020509aff06
RezaMC1999 <rezamc1999@gmail.com>
2024-10-30 00:47:25 +0330

    Improved Project

--- Assets/ScratchMask.cs
+++ Assets/ScratchMask.cs
@@ -7,19 +7,8 @@ public class ScratchMask : MonoBehaviour
     public GameObject waterEffectGO;
     public Animator waterEffectAnimtor;
 
-    private void OnEnable()
+    public void DestroyWaterEffect() 
     {
-        //StartCoroutine(DestroyWaterEffectWithDelay());   
+        Destroy(waterEffectGO);
     }
-
-    //public IEnumerator DestroyWaterEffectWithDelay() 
-    //{
-        //Color transparentColor = new Color(0f, 0f, 0f,0f);
-        //waterEffectSR.enabled = true;
-        //waterEffectSR.color = transparentColor;
-        //yield return new WaitForSeconds(0.25f);
-        //waterEffectSR.color = transparentColor;
-        //waterEffectSR.enabled = false;
-        //Destroy(waterEffectGO);
-    //}
 }
